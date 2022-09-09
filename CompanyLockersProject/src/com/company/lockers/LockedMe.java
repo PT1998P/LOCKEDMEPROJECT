a6bb1bc3655d3920d6ab03db973c9cbd4cab4eeb
PT1998P <pt310320@gmail.com>
2022-09-09 22:14:42 +0530

    lockedmeproject

---
+++ CompanyLockersProject/src/com/company/lockers/LockedMe.java
@@ -0,0 +1,23 @@
+package com.company.lockers;
+
+public class LockedMe 
+{
+
+	public static void main(String[] args) 
+	{
+		displayMenu();
+	}
+public static void displayMenu ()
+{
+	System.out.println("***********************************************************");
+	System.out.println("\tWelcome to Company Lockers -Lockedme.com");
+	System.out.println("\tDeveloper Name: Pradyumn Tiwari");
+	System.out.println("***********************************************************");
+	System.out.println("\t1. Display all the files");
+	System.out.println("\t2. Add files to existing directory");
+	System.out.println("\t3. Delete a file");
+	System.out.println("\t4. Search a file");
+	System.out.println("\t5. Exit");
+	System.out.println("***********************************************************");
+}
+}
